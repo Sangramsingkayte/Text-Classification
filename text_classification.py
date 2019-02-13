9a67fdcb297f423d085d18bf2935f5580e6b48b7
Dr. Sangramsing Kayte <32505428+Sangramsingkayte@users.noreply.github.com>
2019-02-13 12:39:44 +0100

    Add files via upload

---
+++ text_classification.py
@@ -0,0 +1,86 @@
+# Text Xlassification
+
+# Importing the libraries
+
+import numpy as np
+import re
+import pickle
+import nltk
+from nltk.corpus import stopwords
+from sklearn.datasets import load_files
+nltk.download('stopwords')
+
+
+# Importing Dataset
+
+reviews = load_files('txt_sentoken/')
+X,y = reviews.data,reviews.target
+
+
+# Storing as Pickle Files
+
+with open('X.pickle','wb') as f:
+    pickle.dump(X,f)
+    
+with open('y.pickle','wb') as f:
+    pickle.dump(y,f)
+    
+    
+#Unpickling the dataset
+with open('X.pickle','rb') as f:
+    X = pickle.load(f)
+
+with open('X.pickle','rb') as f:
+    y = pickle.load(f)
+    
+
+# Creating the Corpus
+corpus = []
+for i in range(0,len(X)):
+    review = re.sub(r'\W',' ',str(X[i]))
+    review = review.lower()
+    review = re.sub(r'\s+[a-z]\s+',' ',review)
+    review = re.sub(r'^[a-z]\s+',' ',review)
+    review = re.sub(r'\s+',' ',review)
+    corpus.append(review)
+    
+from sklearn.feature_extraction.text import CountVectorizer
+vectorizer = CountVectorizer(max_features=2000,min_df = 3, max_df = 0.6, stop_words = stopwords.words('english'))
+X = vectorizer.fit_transform(corpus).toarray()
+
+
+from sklearn.feature_extraction.text import TfidfTransformer
+transformer = TfidfTransformer()
+X = transformer.fit_transform(X).toarray()
+
+
+
+
+#split dataset 
+from sklearn.model_selection import train_test_split
+text_train,text_test,sent_train,sent_test = train_test_split(X,y,test_size=0.2,random_state = 0)
+
+from sklearn.linear_model import LogisticRegression
+classifier = LogisticRegression()
+classifier.fit(text_train,sent_train)
+    
+
+sent_pred = classifier.predict(text_test)
+
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(sent_test,sent_pred)
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
